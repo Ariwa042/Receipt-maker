70fea3c2313f8e97542f246c4657f23e41de22b7
Ariwa042 <nwezemichael45@gmail.com>
2025-06-12 12:31:21 +0100

    Fix requirements and settings: add crispy_bootstrap4 to installed apps and update requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -36,11 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
     'receipts',
     'users',
     'payments',
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
