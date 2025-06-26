7f64267245cd16420100e56c91583fe8ab4e9819
Ariwa042 <nwezemichael45@gmail.com>
2025-06-26 18:34:56 +0100

    ...tes

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^)_f0m!i+50ri-=s1yflv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('*', 'https://fake-receipt.onrender.com, fake-receipt.onrender.com').split(',')
+ALLOWED_HOSTS = ('fake-receipt.onrender.com')
 
 
 # Application definition
